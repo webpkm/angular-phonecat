4bd8d8a1a9d71ee7e59ae652c9acc9aa54f181c7
pmourya <premkumarmaurya1@gmail.com>
2015-06-26 11:19:15 +0530

    Manully inject the dependency PhoneListCtrl.$inject = ['$scope'];

--- app/js/app.js
+++ app/js/app.js
@@ -3,9 +3,22 @@
 /* App Module */
 angular.module("myApp", [])
 
-.controller('myCtrl', function($scope){
-	$scope.appName = "This is the my app";	
-});
+.controller('PhoneListCtrl', PhoneListCtrl);
+function PhoneListCtrl(s){
+	
+	PhoneListCtrl.$inject = ['$scope'];
+	 s.phones = [
+	                  {'name': 'Nexus S',
+	                   'snippet': 'Fast just got faster with Nexus S.'},
+	                  {'name': 'Motorola XOOM™ with Wi-Fi',
+	                   'snippet': 'The Next, Next Generation tablet.'},
+	                  {'name': 'MOTOROLA XOOM™',
+	                   'snippet': 'The Next, Next Generation tablet.'}
+	                ];
+	 
+	s.appName = "This is the my app";	
+}
+PhoneListCtrl.$inject = ['$scope'];
 
 angular.element(document).ready(function() {
 	angular.bootstrap(document, ['myApp']);
