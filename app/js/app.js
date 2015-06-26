1a6586b4eac51c1dd938585b7e7192e3845efc69
pmourya <premkumarmaurya1@gmail.com>
2015-06-26 11:05:08 +0530

    Automatically bootstrapping angular.element(document).ready(function() { 	angular.bootstrap(document, ['myApp']); });

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,12 @@
 'use strict';
 
 /* App Module */
+angular.module("myApp", [])
+
+.controller('myCtrl', function($scope){
+	$scope.appName = "This is the my app";	
+});
+
+angular.element(document).ready(function() {
+	angular.bootstrap(document, ['myApp']);
+});
